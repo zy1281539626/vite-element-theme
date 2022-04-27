aa13a823e431957754475a0ede5eda3cfe6122aa
张营 <zy_carl@formssi.com>
2022-04-27 17:39:34 +0800

    update

--- README.md
+++ README.md
@@ -1,7 +1,42 @@
-# Vue 3 + Vite
+# element-plus 主题修改方案
 
-This template should help get you started developing with Vue 3 in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
+## 1. 覆盖 element scss 变量，使用 vite 配置 additionalData
 
-## Recommended IDE Setup
+- 主题颜色只能写死，只配置一次
 
-- [VS Code](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=johnsoncodehk.volar)
+```js
+// element.scss 覆盖变量
+@forward "element-plus/theme-chalk/src/common/var.scss" with
+(
+    // do not use same name, it will override.
+    $colors: $--colors,
+    $button-padding-horizontal: ("default": 50px)
+);
+
+// vite配置
+css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "~/styles/theme/element.scss" as *;`,
+      },
+    },
+},
+```
+
+## 2. 覆盖 :root css 变量 (覆盖的样式会全部显示在 html 标签上)
+
+- 可动态修改 root 变量，需要计算相关的颜色值
+
+```js
+import { useCssVar } from "@vueuse/core";
+const color = useCssVar("--el-color-primary");
+color.value = "red";
+// 这里只修改了primary-color的一种颜色，其他颜色要算
+```
+
+## 3. vite 配置可修改颜色值，编译后替换 css
+
+借助插件 viteThemePlugin （vben antd）
+
+- vite 配置 element 原来可修改的相关颜色值数组
+- 也可配置自定义的值
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -17,11 +17,90 @@
     <el-button type="warning" plain>Warning</el-button>
     <el-button type="danger" plain>Danger</el-button>
   </el-row>
+  <el-row>
+    <el-button type="primary">切换主题</el-button>
+  </el-row>
 </template>
 
 <script>
-export default {
+import { generateColors, chalk } from '@element-plus/colors'
 
+import { useCssVar } from '@vueuse/core'
+export default {
+  setup(){
+    const color = useCssVar('--el-color-primary')
+    color.value = "red"
+    
+    function colorRGB2Hex(color) {
+      var rgb = color.split(',');
+      var r = parseInt(rgb[0].split('(')[1]);
+      var g = parseInt(rgb[1]);
+      var b = parseInt(rgb[2].split(')')[0]);
+      var hex = "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+      return hex;
+    }
+
+    let { primary, success, warning, danger, error, info } = generateColors('#aa1696')
+    primary = primary.map(item=>{
+      return colorRGB2Hex(item)
+    })
+    // console.log([primary[0],primary[3],primary[5],primary[7],primary[8],primary[9]])
+
+    success = success.map(item=>{
+      return colorRGB2Hex(item)
+    })
+    // console.log(success)
+    // console.log(chalk)
+
+
+    // ================================================================================
+    const colorMix = (color1, color2, weight) => {
+      weight = Math.max(Math.min(Number(weight), 1), 0)
+      let r1 = parseInt(color1.substring(1, 3), 16)
+      let g1 = parseInt(color1.substring(3, 5), 16)
+      let b1 = parseInt(color1.substring(5, 7), 16)
+      let r2 = parseInt(color2.substring(1, 3), 16)
+      let g2 = parseInt(color2.substring(3, 5), 16)
+      let b2 = parseInt(color2.substring(5, 7), 16)
+      let r = Math.round(r1 * (1 - weight) + r2 * weight)
+      let g = Math.round(g1 * (1 - weight) + g2 * weight)
+      let b = Math.round(b1 * (1 - weight) + b2 * weight)
+      r = ("0" + (r || 0).toString(16)).slice(-2)
+      g = ("0" + (g || 0).toString(16)).slice(-2)
+      b = ("0" + (b || 0).toString(16)).slice(-2)
+      return "#" + r + g + b;
+    }
+
+    let colorObj = {
+      '--el-color-white': '#ffffff',
+      '--el-color-primary': '#409eff'
+    }
+
+    for (let i = 1; i < 10; i += 1) {
+      colorObj[`--el-color-primary-light-${10 - i}`] = colorMix('#ffffff', colorObj['--el-color-primary'], i * 0.1)
+    }
+    // console.log(colorObj)
+
+    // dark ================================================================
+    const darkColor = (color1, color2, weight) => {
+      weight = Math.max(Math.min(Number(weight), 1), 0)
+      let r1 = parseInt(color1.substring(1, 3), 16)
+      let g1 = parseInt(color1.substring(3, 5), 16)
+      let b1 = parseInt(color1.substring(5, 7), 16)
+      let r2 = parseInt(color2.substring(1, 3), 16)
+      let g2 = parseInt(color2.substring(3, 5), 16)
+      let b2 = parseInt(color2.substring(5, 7), 16)
+      let r = Math.round(r1 + r2 * weight)
+      let g = Math.round(g1 + g2 * weight)
+      let b = Math.round(b1 + b2 * weight)
+      r = ("0" + (r || 0).toString(16)).slice(-2)
+      g = ("0" + (g || 0).toString(16)).slice(-2)
+      b = ("0" + (b || 0).toString(16)).slice(-2)
+      return "#" + r + g + b;
+    }
+    let aa = darkColor('#141414', '#409eff', 0.1)
+    console.log(aa)
+  }
 }
 </script>
 
